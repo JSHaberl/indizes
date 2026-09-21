0498a6058c1fcbc0c4d7b7c54bd5815fde86c1dc
JSHaberl <sebastian.haberl@proton.me>
2026-09-21 12:58:52 +0200

    Added first iteration of faker-loop for name-gen

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import sqlite3
 
+
+genrange = range(500000)
+
+con = sqlite3.connect("mydb.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE personen(id, vorname, nachname)")
+
+for x in genrange:
+    cur.execute("INSERT INTO personen(vorname,nachname) VALUES(?, ?)", faker.name().split()) 
+    con.commit()
+    
+    
 print("Test")
