e7ed83228579a2e7aa1cc78e36fdf08f910ea055
JSHaberl <sebastian.haberl@proton.me>
2026-09-22 17:02:38 +0200

    Added logic to calculate and print the variance

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import sqlite3
+from faker import Faker
 
-
-genrange = range(500000)
+faker = Faker()
+genbase = 500000
+genvalue = genbase
 
 con = sqlite3.connect("mydb.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE personen(id, vorname, nachname)")
+cur.execute("CREATE TABLE IF NOT EXISTS personen(id, vorname, nachname)")
+fillcheck = cur.execute("SELECT COUNT(*) FROM personen").fetchall()[0][0]
 
-for x in genrange:
-    cur.execute("INSERT INTO personen(vorname,nachname) VALUES(?, ?)", faker.name().split()) 
+print(fillcheck)
+
+if genvalue - fillcheck > 0:
+    genvalue -= fillcheck
+    for x in range(genvalue):
+        cur.execute("INSERT INTO personen(vorname,nachname) VALUES(?, ?)", (faker.first_name(), faker.last_name()),)
     con.commit()
     
-    
-print("Test")
+uniquenames = cur.execute("SELECT DISTINCT(concat(vorname, ' ', nachname)) FROM personen").fetchall()
+uniquenamestotal = len(uniquenames)
+uniquenamescount = cur.execute("SELECT COUNT(*) FROM personen GROUP BY concat(vorname, ' ', nachname)").fetchall()
+avg = genbase / uniquenamestotal
+summe = 0
+for x in range(uniquenamestotal):
+   summe += (uniquenamescount[x][0] - avg)**2
+
+
+print(f"Varianz: {summe/genbase}")
+con.close()
